cd7c9ec3a62b81074e627982ad2c8afee7310623
Thomas Aregger <thomas.aregger@gmail.com>
2013-12-17 23:43:56 +0100

    Disable SOS Button in AwaitingState

--- src/ch/ffhs/esa/lifeguard/ui/AwaitingView.java
+++ src/ch/ffhs/esa/lifeguard/ui/AwaitingView.java
@@ -1,7 +1,9 @@
 package ch.ffhs.esa.lifeguard.ui;
 
+import ch.ffhs.esa.lifeguard.R;
 import android.app.Activity;
 import android.content.Intent;
+import android.widget.Button;
 
 public class AwaitingView
     implements ViewStateStrategy
@@ -9,8 +11,14 @@ public class AwaitingView
     @Override
     public void handleUi (Activity activity, Intent intent)
     {
-//        Long id = Long.valueOf (intent.getExtras ().get ("rescuer").toString ());
-//        Contacts contacts = new Contacts (Lifeguard.getDatabaseHelper ());
-//        ContactInterface contact = contacts.findById (id);
+        Button button = (Button) activity.findViewById (R.id.SOSButton);
+        button.setEnabled (false);
+        button = (Button) activity.findViewById (R.id.toggleButtonAlarmSwitch);
+        button.setEnabled (false);
+        
+        //Long id = Long.valueOf (intent.getExtras ().get ("rescuer").toString ());
+        //Contacts contacts = new Contacts (Lifeguard.getDatabaseHelper ());
+        //ContactInterface contact = contacts.findById (id);
+        
     }
 }
