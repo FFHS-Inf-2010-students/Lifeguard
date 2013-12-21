914eefe3741653a801e4e48eecbaf42895c3fd65
Christof Kälin <christof.kaelin@students.ffhs.ch>
2013-12-21 19:23:42 +0100

    Added testcase listing including manual tests and made some minor changes.

--- src/ch/ffhs/esa/lifeguard/ui/InitialView.java
+++ src/ch/ffhs/esa/lifeguard/ui/InitialView.java
@@ -1,17 +1,20 @@
 package ch.ffhs.esa.lifeguard.ui;
 
-import ch.ffhs.esa.lifeguard.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import ch.ffhs.esa.lifeguard.R;
 
-public class InitialView
-    implements ViewStateStrategy
-{
-    @Override
-    public void handleUi (Activity activity, Intent intent)
-    {
-        Button button = (Button) activity.findViewById (R.id.SOSButton);
-        button.setEnabled (true);
-    }
+public class InitialView implements ViewStateStrategy {
+	@Override
+	public void handleUi(Activity activity, Intent intent) {
+		Button button = (Button) activity.findViewById(R.id.SOSButton);
+		button.setEnabled(false);
+		button = (Button) activity.findViewById(R.id.toggleButtonAlarmSwitch);
+		button.setEnabled(false);
+		ProgressBar bar = (ProgressBar) activity
+				.findViewById(R.id.progressBarDelay);
+		bar.setProgress(0);
+	}
 }
