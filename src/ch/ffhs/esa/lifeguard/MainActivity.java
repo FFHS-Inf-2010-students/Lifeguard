f4c54dcca611ab6e384afaa8f02bee97ff91c9d3
David Daniel <fidel@daniels.li>
2013-12-23 17:53:47 +0100

    Correct indentation in main activity

--- src/ch/ffhs/esa/lifeguard/MainActivity.java
+++ src/ch/ffhs/esa/lifeguard/MainActivity.java
@@ -27,129 +27,129 @@ import ch.ffhs.esa.lifeguard.ui.ViewStrategyFactory;
  * 
  */
 public class MainActivity extends Activity {
-	AlarmService alarmService;
-	boolean bound = false;
+    AlarmService alarmService;
+    boolean bound = false;
 
-	private BroadcastReceiver stateChangeReceiver = new BroadcastReceiver() {
-		public void onReceive(Context context, Intent intent) {
-			onStateChanged(intent);
-		}
-	};
+    private BroadcastReceiver stateChangeReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            onStateChanged(intent);
+        }
+    };
 
-	private ViewStrategyFactory viewStrategyFactory = new ViewStrategyFactory();
+    private ViewStrategyFactory viewStrategyFactory = new ViewStrategyFactory();
 
-	/*
-	 * //////////////////////////////////////////////////////////////////////////
-	 * CREATION
-	 */
+    /*
+     * //////////////////////////////////////////////////////////////////////////
+     * CREATION
+     */
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		registerReceiver(stateChangeReceiver, new IntentFilter(
-				ServiceMessage.CURRENT_SERVICE_STATE));
+        registerReceiver(stateChangeReceiver, new IntentFilter(
+                    ServiceMessage.CURRENT_SERVICE_STATE));
 
-		Intent intent = new Intent(this, AlarmService.class);
-		Log.d(MainActivity.class.toString(), "Start Service");
-		startService(intent);
+        Intent intent = new Intent(this, AlarmService.class);
+        Log.d(MainActivity.class.toString(), "Start Service");
+        startService(intent);
 
-		Log.d(MainActivity.class.toString(), "Before Bind");
-		bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
-		Log.d(MainActivity.class.toString(), "After Bind");
-	}
+        Log.d(MainActivity.class.toString(), "Before Bind");
+        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+        Log.d(MainActivity.class.toString(), "After Bind");
+    }
 
-	@Override
-	protected void onStart() {
-		super.onStart();
+    @Override
+    protected void onStart() {
+        super.onStart();
 
-		if (alarmService == null) {
-			Log.d(MainActivity.class.toString(),
-					"AlarmService not started yet.");
-		} else {
-			// Log.d(MainActivity.class.toString(),
-			// alarmService.getState().toString());
-		}
-	}
+        if (alarmService == null) {
+            Log.d(MainActivity.class.toString(),
+                    "AlarmService not started yet.");
+        } else {
+            // Log.d(MainActivity.class.toString(),
+            // alarmService.getState().toString());
+        }
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return super.onCreateOptionsMenu(menu);
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
-	/*
-	 * //////////////////////////////////////////////////////////////////////////
-	 * EVENT HANDLING
-	 */
+    /*
+     * //////////////////////////////////////////////////////////////////////////
+     * EVENT HANDLING
+     */
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case R.id.action_configuration:
-			openConfiguration();
-			return true;
-		case R.id.action_contact_list:
-			viewContacts();
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
-		}
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_configuration:
+                openConfiguration();
+                return true;
+            case R.id.action_contact_list:
+                viewContacts();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
-	@Override
-	public void onDestroy() {
-		unregisterReceiver(stateChangeReceiver);
-		unbindService(serviceConnection);
-		viewStrategyFactory.notifiyClose();
-		super.onDestroy();
-	}
+    @Override
+    public void onDestroy() {
+        unregisterReceiver(stateChangeReceiver);
+        unbindService(serviceConnection);
+        viewStrategyFactory.notifiyClose();
+        super.onDestroy();
+    }
 
-	/*
-	 * //////////////////////////////////////////////////////////////////////////
-	 * ACTIONS
-	 */
+    /*
+     * //////////////////////////////////////////////////////////////////////////
+     * ACTIONS
+     */
 
-	/**
-	 * Starts the configuration activity.
-	 */
-	public void openConfiguration() {
-		Intent intent = new Intent(this, ConfigurationActivity.class);
-		startActivity(intent);
-	}
+    /**
+     * Starts the configuration activity.
+     */
+    public void openConfiguration() {
+        Intent intent = new Intent(this, ConfigurationActivity.class);
+        startActivity(intent);
+    }
 
-	/**
-	 * Starts the contact list activity to display all available contacts.
-	 */
-	public void viewContacts() {
-		Intent intent = new Intent(this, ContactListActivity.class);
-		startActivity(intent);
-	}
+    /**
+     * Starts the contact list activity to display all available contacts.
+     */
+    public void viewContacts() {
+        Intent intent = new Intent(this, ContactListActivity.class);
+        startActivity(intent);
+    }
 
-	private ServiceConnection serviceConnection = new ServiceConnection() {
-		@Override
-		public void onServiceConnected(ComponentName className, IBinder service) {
-			AlarmBinder binder = (AlarmBinder) service;
-			alarmService = binder.getService();
-			bound = true;
-		}
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            AlarmBinder binder = (AlarmBinder) service;
+            alarmService = binder.getService();
+            bound = true;
+        }
 
-		@Override
-		public void onServiceDisconnected(ComponentName arg0) {
-			bound = false;
-		}
-	};
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            bound = false;
+        }
+    };
 
-	private void onStateChanged(Intent intent) {
-		Bundle bundle = intent.getExtras();
+    private void onStateChanged(Intent intent) {
+        Bundle bundle = intent.getExtras();
 
-		AlarmStateId current = AlarmStateId.valueOf(AlarmStateId.class,
-		        bundle.getString (ServiceMessage.Key.ALARM_STATE_ID));
+        AlarmStateId current = AlarmStateId.valueOf(AlarmStateId.class,
+                bundle.getString (ServiceMessage.Key.ALARM_STATE_ID));
 
-		Log.d(getClass().toString(), "State Change: " + current.toString());
+        Log.d(getClass().toString(), "State Change: " + current.toString());
 
-		viewStrategyFactory.create(current).handleUi(this, intent);
-	}
+        viewStrategyFactory.create(current).handleUi(this, intent);
+    }
 }
